6eed7e79399f5d3f567f04ab821bf24da80cf7a0
inga-divra <106383378+inga-divra@users.noreply.github.com>
2025-12-11 20:02:22 +0200

    Tehtävä 13.5 done.

---
+++ controllers/blogs.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const { Blog } = require('../models');
+
+router.get('/', async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body);
+    res.status(201).json(blog);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog) {
+    await blog.destroy();
+  }
+  res.status(204).end();
+});
+
+module.exports = router;
